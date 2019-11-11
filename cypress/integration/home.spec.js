b1f55d7fc6fd6d6a72f5fef226a1b29f07f541e2
pabloturati <pablo.turati@gmail.com>
2019-11-11 07:11:10 -0500

    Implemented Cypress testing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -6,11 +6,32 @@ describe('Visit Root (Home page)', () => {
   it('Home page is rendered', () => {
     cy.url().should('include', routes.home)
   })
-  // it('Contains "description" section', () => hasDescriptionSection)
-  // it('contains hero', () => {
-  //   cy.get('div.p__home__title__box')
-  //     .and('is.visible')
-  //     .and('has.length', 1)
-  // })
-  // it('Contains card module', hasStackCardModule)
+  it('Contains header', () => {
+    cy.get('header')
+      .should('be.visible')
+      .and('has.length', 1)
+      .and('have.class', 'o__header')
+  })
+  it('Header must contain logo, button and title', () => {
+    cy.get('header')
+      .children()
+      .should('have.length', 2)
+    cy.get('header')
+      .children()
+      .first()
+      .should('have.class', 'o__header__titles')
+      .and('have.length', 1)
+    cy.get('.o__header__titles').should('have.length', 1)
+    cy.get('.o__header__titles')
+      .children('a')
+      .should('have.length', 1)
+    cy.get('div.o__header__titles').should($child => {
+      expect(
+        $child
+          .children('a')
+          .first()
+          .children()
+      ).to.have.length(1)
+    })
+  })
 })
--- cypress/support/testConfig.js
+++ cypress/support/testConfig.js
@@ -16,21 +16,6 @@ export const navigateTo = (route, verifyCallback) => {
 
 export const initializeViewAndServer = () => {
   cy.viewport(viewport)
-  // cy.server()
-  // cy.route(`${API_URL}global`).as('getApiGlobal')
-  // cy.route(`${API_URL}website`).as('getApiWebsite')
-  // cy.route('POST', `${API_URL}${API_ROUTES.registerBrand}`).as('registerBrand')
-  // cy.route('POST', `${API_URL}${API_ROUTES.registerAgency}`).as(
-  //   'registerAgency'
-  // )
-  // cy.route('PUT', `${API_URL}${API_ROUTES.updateAgencyRegister}/**`).as(
-  //   'updateRegisterAgency'
-  // )
-  // cy.route('POST', `${API_URL}${API_ROUTES.influencers}`).as(
-  //   'registerInfluencer'
-  // )
-  // cy.route(`${API_URL}${API_ROUTES.tiers}`).as('getTiers')
-  // cy.route(`${API_URL}${API_ROUTES.profile}`).as('getProfile')
 }
 
 export const initializeViewAndNavigateTo = route => {
